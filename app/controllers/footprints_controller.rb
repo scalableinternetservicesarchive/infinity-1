18cc9b5f8c3d9492fa27e71de4617fdf1b111c9d
maulishah91 <maulishah91@gmail.com>
2015-10-22 19:19:52 -0700

    User sign up page and sample hello user page

--- app/controllers/footprints_controller.rb
+++ app/controllers/footprints_controller.rb
@@ -1,4 +1,10 @@
 class FootprintsController < ApplicationController
   def index
+    @user =  current_user
   end
+  
+  def show
+    @user = current_user
+  end
+
 end
---
+++ db/migrate/20151023005859_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :interests, :string
+    add_column :users, :date_of_birth, :datetime
+    add_column :users, :is_female, :boolean, default: false
+  end
+end

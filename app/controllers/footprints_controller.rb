cc75252d40a13e5fd680b0fa11b3b6682959d979
maulishah91 <maulishah91@gmail.com>
2015-10-22 19:33:10 -0700

    Structure for users for user related functionality. - Created dummy profile page for user

--- app/controllers/footprints_controller.rb
+++ app/controllers/footprints_controller.rb
@@ -2,9 +2,5 @@ class FootprintsController < ApplicationController
   def index
     @user =  current_user
   end
-  
-  def show
-    @user = current_user
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only => [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'footprints#index'
-   get 'footprints/show' => 'footprints#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

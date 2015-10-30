44960ba4e5ba3ad5b67b32356d69e71795b1b5ce
Avneet <avneetoberoi15@gmail.com>
2015-10-30 11:53:37 -0700

    Category added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   devise_for :users, :path_prefix => 'd'
   resources :users, :only => [:show]
   resources :notes
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030184056) do
+ActiveRecord::Schema.define(version: 20151030185214) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "footprints", force: :cascade do |t|
     t.datetime "created_at", null: false

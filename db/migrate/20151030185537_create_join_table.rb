5c9eabdd810b3ca2b3edd6d89e8d74bb926c4829
Avneet <avneetoberoi15@gmail.com>
2015-10-30 11:57:02 -0700

    Association added between users and categories

---
+++ db/migrate/20151030185537_create_join_table.rb
@@ -0,0 +1,8 @@
+class CreateJoinTable < ActiveRecord::Migration
+  def change
+    create_join_table :users, :categories do |t|
+       t.index [:user_id, :category_id]
+       t.index [:category_id, :user_id]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030185214) do
+ActiveRecord::Schema.define(version: 20151030185537) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "categories_users", id: false, force: :cascade do |t|
+    t.integer "user_id",     null: false
+    t.integer "category_id", null: false
+  end
+
+  add_index "categories_users", ["category_id", "user_id"], name: "index_categories_users_on_category_id_and_user_id"
+  add_index "categories_users", ["user_id", "category_id"], name: "index_categories_users_on_user_id_and_category_id"
 
   create_table "footprints", force: :cascade do |t|
     t.datetime "created_at", null: false

821b11bf7edfec8f7d8530910cc3d562a83e5fbe
maulishah91 <maulishah91@gmail.com>
2015-11-04 23:25:49 -0800

    notes have city_name column. Made that change

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -71,6 +71,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:title, :city, :content, :tags)
+      params.require(:note).permit(:title, :city_name, :content, :tags)
     end
 end

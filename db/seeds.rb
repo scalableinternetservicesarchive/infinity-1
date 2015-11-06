e875cdc75b91375e6b05cf19366c0c7328d4291a
maulishah91 <maulishah91@gmail.com>
2015-11-06 15:12:25 -0800

    Pretty pretty comments in blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,13 @@ Category.create(name: 'Hiking')
 Category.create(name: 'Delicious food')
 Category.create(name: 'Amusement parks')
 Category.create(name: 'Trekking')
+
+
+City.create(city_name: 'Los Angeles', city_desc: 'New city')
+
+
+Blog.create(city_name: 'Los Angeles', title:'Long Weekend @ LA',body: 'The United States’ second biggest city has got it all. Beaches, boulevards, baseball and Beverly hills.',
+            link: 'http://www.urbantravelblog.com/guide/los-angeles/', picture: File.open(File.join(Rails.root, '/app/assets/images/LosAngelesBlog1.jpg')), author: 'Vince Robbins');
+
+Blog.create(city_name: 'Paris', title:'The Hip Paris Blog',body: 'Le Procope: A Paris Dining Experience with a Side of French History.',
+            link: 'http://hipparis.com/2015/10/13/le-procope-a-paris-dining-experience-with-a-side-of-french-history/', picture: File.open(File.join(Rails.root, '/app/assets/images/HipParis.jpg')), author: 'Emily Dillin');

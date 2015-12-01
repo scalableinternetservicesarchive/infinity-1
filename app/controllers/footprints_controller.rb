a246d52f76b14faa51ed870a6941d94c3604d30b
Avneet <avneetoberoi15@gmail.com>
2015-11-30 23:25:30 -0800

    Pagination for news feed

--- app/controllers/footprints_controller.rb
+++ app/controllers/footprints_controller.rb
@@ -2,8 +2,8 @@ class FootprintsController < ApplicationController
   def index
     @user =  current_user
     @user_interests = @user.categories.select('id')
-    @notes_feed = Note.where("id in (select note_id from categories_notes inner join categories where categories_notes.category_id = categories.id and categories.id in (?))",@user_interests)
-    @blogs_feed = Blog.where("id in (select blog_id from blogs_categories inner join categories where blogs_categories.category_id = categories.id and categories.id in (?))",@user_interests)
+    @notes_feed = Note.where("id in (select note_id from categories_notes inner join categories where categories_notes.category_id = categories.id and categories.id in (?))",@user_interests).paginate(:page => params[:page], :per_page => 5)
+    @blogs_feed = Blog.where("id in (select blog_id from blogs_categories inner join categories where blogs_categories.category_id = categories.id and categories.id in (?))",@user_interests).paginate(:page => params[:page], :per_page => 5)
   end
 
 end

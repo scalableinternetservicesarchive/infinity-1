e8aa3895a927c361fa3c3977767bff3d2b4a0016
yashaswialladi <yashaswialladi@gmail.com>
2015-12-02 02:09:21 -0800

    Merge remote-tracking branch 'remotes/origin/DbOptimization' into allOptimizations
    Conflicts:
    	Gemfile
    	app/controllers/categories_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,109 +11,111 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106071752) do
+ActiveRecord::Schema.define(version: 20151123032325) do
 
   create_table "blog_comments", force: :cascade do |t|
-    t.integer  "blog_id",    limit: 4
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "blog_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
-  add_index "blog_comments", ["blog_id"], name: "index_blog_comments_on_blog_id", using: :btree
+  add_index "blog_comments", ["blog_id"], name: "index_blog_comments_on_blog_id"
 
   create_table "blogs", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "body",       limit: 65535
-    t.string   "city_name",  limit: 255
-    t.string   "author",     limit: 255
-    t.string   "link",       limit: 255
-    t.string   "picture",    limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "user_id",    limit: 4
+    t.string   "title"
+    t.text     "body"
+    t.string   "city_name"
+    t.string   "author"
+    t.string   "link"
+    t.string   "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "blogs_categories", id: false, force: :cascade do |t|
-    t.integer "blog_id",     limit: 4, null: false
-    t.integer "category_id", limit: 4, null: false
+    t.integer "blog_id",     null: false
+    t.integer "category_id", null: false
   end
 
-  add_index "blogs_categories", ["blog_id", "category_id"], name: "index_blogs_categories_on_blog_id_and_category_id", using: :btree
-  add_index "blogs_categories", ["category_id", "blog_id"], name: "index_blogs_categories_on_category_id_and_blog_id", using: :btree
+  add_index "blogs_categories", ["blog_id", "category_id"], name: "index_blogs_categories_on_blog_id_and_category_id"
+  add_index "blogs_categories", ["category_id", "blog_id"], name: "index_blogs_categories_on_category_id_and_blog_id"
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories_notes", id: false, force: :cascade do |t|
-    t.integer "note_id",     limit: 4, null: false
-    t.integer "category_id", limit: 4, null: false
+    t.integer "note_id",     null: false
+    t.integer "category_id", null: false
   end
 
-  add_index "categories_notes", ["category_id", "note_id"], name: "index_categories_notes_on_category_id_and_note_id", using: :btree
-  add_index "categories_notes", ["note_id", "category_id"], name: "index_categories_notes_on_note_id_and_category_id", using: :btree
+  add_index "categories_notes", ["category_id", "note_id"], name: "index_categories_notes_on_category_id_and_note_id"
+  add_index "categories_notes", ["note_id", "category_id"], name: "index_categories_notes_on_note_id_and_category_id"
 
   create_table "categories_users", id: false, force: :cascade do |t|
-    t.integer "user_id",     limit: 4, null: false
-    t.integer "category_id", limit: 4, null: false
+    t.integer "user_id",     null: false
+    t.integer "category_id", null: false
   end
 
-  add_index "categories_users", ["category_id", "user_id"], name: "index_categories_users_on_category_id_and_user_id", using: :btree
-  add_index "categories_users", ["user_id", "category_id"], name: "index_categories_users_on_user_id_and_category_id", using: :btree
+  add_index "categories_users", ["category_id", "user_id"], name: "index_categories_users_on_category_id_and_user_id"
+  add_index "categories_users", ["user_id", "category_id"], name: "index_categories_users_on_user_id_and_category_id"
 
   create_table "cities", force: :cascade do |t|
-    t.string   "city_name",  limit: 255
-    t.string   "city_desc",  limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "city_name"
+    t.string   "city_desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "note_id",    limit: 4
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "note_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
-  add_index "comments", ["note_id"], name: "index_comments_on_note_id", using: :btree
+  add_index "comments", ["note_id"], name: "index_comments_on_note_id"
 
   create_table "notes", force: :cascade do |t|
-    t.string   "city_name",  limit: 255
-    t.string   "title",      limit: 255
-    t.text     "content",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "user_id",    limit: 4
+    t.string   "city_name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "interests",              limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "interests"
     t.datetime "date_of_birth"
-    t.boolean  "is_female",                          default: false
-    t.string   "quote",                  limit: 255
-    t.string   "picture",                limit: 255
+    t.boolean  "is_female",              default: false
+    t.string   "quote"
+    t.string   "picture"
+    t.integer  "blog_comment_id"
+    t.integer  "comment_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  add_foreign_key "blog_comments", "blogs"
-  add_foreign_key "comments", "notes"
 end
